a15a5e6a750b14f7709a2db8f1101e3e4deb1c54
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-04-26 19:42:34 -0700

    Removed files that were accidentally uploaded from project-amico v1

--- app/public/assets/js/session.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function(){
-
-  $(document).on('click','.logout-action',function(){
-    firebase.auth().signOut();
-    window.location = '/';
-    console.log('SIGNED OUT!');
-  })    
-});
