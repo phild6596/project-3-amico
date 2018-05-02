93c1c3d97674fc8ff630690d25dec77ad906ab32
phild6596 <phild6596@gmail.com>
2018-05-01 19:10:00 -0700

    added postTopic.js to client side

--- client/src/components/topicButton.js
+++ client/src/components/topicButton.js
@@ -4,3 +4,5 @@ export const TopicButton = props =>
     <button {...props} type="button" class="btn btn-outline-primary postButton create-topic" style={{ marginLeft:4 }} onClick={()=> {onClick()}}>
         Post
     </button>;
+
+export default TopicButton;
---
+++ client/src/pages/Home/postTopic.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const createNewTopic = (text) => {
+    if(firebase.auth().currentUser){
+        firebase.auth().currentUser.getIdToken(true).then((getIdToken)=> {
+            axios.post('/topic', {topic:{text: 'Hello World'}, idToken:idToken}).then((data)=> {
+                console.log(data);
+                //getTopics();
+            });
+        }).catch((error)=> {
+            console.log('Error...', error);
+        });
+    }
+    else{
+        console.log('Not Logged in...');
+    }
+}
