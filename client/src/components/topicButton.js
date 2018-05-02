7662e22ce428bac7f48adca6ab78b5232bde46ab
phild6596 <phild6596@gmail.com>
2018-05-01 17:56:23 -0700

    changed messageButton.js to topicButton.js

--- client/src/components/topicButton.js
+++ client/src/components/topicButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const MessageButton = props =>
+export const TopicButton = props =>
     <button {...props} type="button" class="btn btn-outline-primary postButton create-topic" style={{ marginLeft:4 }} onClick={()=> {onClick()}}>
         Post
     </button>;
