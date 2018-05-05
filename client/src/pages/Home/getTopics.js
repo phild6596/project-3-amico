1cab0efde2f8a5d5d6cd0604e92e3230cbaf136a
phild6596 <phild6596@gmail.com>
2018-05-05 10:34:59 -0700

    added getTopics.js to Home.js

---
+++ client/src/pages/Home/getTopics.js
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios";
+import firebase from "firebase";
+
+const getTopics = () => {
+    let topics = [];
+
+    if(firebase.auth().currentUser) {
+        firebase.auth().currentUser.getIdToken(true).then((idToken) => {
+            axios.post("/topic/search", {language:"english", idToken:idToken})
+            .then((data) => {
+                topics = [];
+                console.log(data);
+                data.map((topic) => {
+                    //topics.push(formatopic(topic));
+                })
+                //topicContainer.empty();
+                //topicContainer.append(topics);
+            });
+        }).catch((error) => {
+            console.log("Error in getTopics.js", error);
+        })
+    }
+    else{
+        console.log("Not logged in...");
+    }
+}
+
+export default getTopics;
