e2d63efddc835dfb4222e28af2a073a9242688c0
phild6596 <phild6596@gmail.com>
2018-05-05 10:47:43 -0700

    small cosmetic changes to createNewTopic.js

--- client/src/pages/Home/createNewTopic.js
+++ client/src/pages/Home/createNewTopic.js
@@ -9,7 +9,7 @@ const createNewTopic = (text) => {
         firebase.auth().currentUser.getIdToken(true).then((idToken)=> {
             axios.post('/topic', {topic:{text:text}, idToken:idToken}).then((data)=> {
                 console.log(data);
-                //getTopics();
+                getTopics();
             });
         }).catch((error)=> {
             console.log('Error...', error);
