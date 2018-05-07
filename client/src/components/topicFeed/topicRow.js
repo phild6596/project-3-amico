18892906510d0617768f91c29cfc825a59f6a2ea
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-05-07 00:52:26 -0700

    TopicRows working, realtime topic posting and updating of page, sticky header, accessible user profiles based on avatar photo or name click, need to clear Input Bar aka topic input after topic is posted

---
+++ client/src/components/topicFeed/topicRow.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styling from "./topicRow.css"
+
+export const TopicRow = props => {
+  return (
+        <div className="topic-row">
+        
+            <a href={"/profile/" + props.topic.authorId}>
+                <img src={props.topic.authorAvatarUrl} alt="user_pic" className="rounded-circle img-responsive topic-avatar-photo"/>
+                 
+            </a>
+            <a href={"/profile/" + props.topic.authorId}>
+                <span className="topic-author-name">{props.topic.authorName}</span>
+            </a>
+            <div className="topic-text">{props.topic.text}</div>
+            <div className="topic-timestamp">{props.topic.timestamp}</div>  
+        </div>
+  );
+}
+export default TopicRow;
