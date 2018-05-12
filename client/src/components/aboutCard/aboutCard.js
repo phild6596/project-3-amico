177a5785c5fd190633ea0966bc2446bbf119f15c
haigbedros <haigbedros@gmail.com>
2018-05-12 08:30:31 -0700

    final home page

--- client/src/components/aboutCard/aboutCard.js
+++ client/src/components/aboutCard/aboutCard.js
@@ -3,15 +3,16 @@ import {Image} from 'react-bootstrap';
 import './aboutCard.css';
 export const AboutCard = (props) => {
     return(
-
-        <div className= "row">
+        <div className= "row">            
             <div className="col-md-12 col-sm-12">
-                <div className="card card-profile mb-4">
+            <div className="card-profile">
+                <div className="card mb-4">
                 <div className="card-header"></div>
                     <div className = "card-body text-center">
                         <Image src={props.user.avatarUrl} className="card-profile-img"/>
-                        <h3>{props.user.displayName}</h3>
+                        <h3 className="text-inherit current-user-name">{props.user.displayName}</h3>
                     </div>
+                </div>
                 </div>
 
                 <div className="card d-md-block d-md-block mb-4">
@@ -22,7 +23,7 @@ export const AboutCard = (props) => {
                                 <li><span className="text-muted icon glyphicon glyphicon-map-marker mr-3"></span> From<span className="user_props"> {props.user.from}</span></li>
                                 <li><span className="text-muted icon glyphicon glyphicon-globe mr-3"></span> Native Language<span className="user_props"> {props.user.nativeLanguage}</span></li>
                                 <li><span className="text-muted icon glyphicon glyphicon-education mr-3"></span> Studying<span className="user_props"> {props.user.studiedLanguage}</span></li>
-                                <li><span className="text-muted icon glyphicon glyphicon-user mr-3"></span> UID <span className="user_props"> {props.user.uid} </span></li>
+                                <li><span className="text-muted icon glyphicon glyphicon-user mr-3"></span> UID <span className="user_props user_id"> {props.user.uid} </span></li>
                             </ul>
                     </div>
                 </div>
