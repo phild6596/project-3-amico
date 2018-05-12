9fb685b7ccaa8dfb51d2f725ff9fe44b805d0266
haigbedros <haigbedros@gmail.com>
2018-05-11 22:38:07 -0700

    homepage user card

--- client/src/components/topicFeed/topicInput.js
+++ client/src/components/topicFeed/topicInput.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export const TopicInput = props => {
   return (
-        <div className="col-md-12 col-sm-12">
+        <div className="col-md-6 col-sm-12">
            <form acceptCharset="UTF-8">
              <input className="expand form-control message-input-button-position" id="topic-text-box" {...props} rows="1" />
              <div className="float-right">
