88c1a5cc8a3a74eeae627ae32d7fe848d8ba7a85
haigbedros <haigbedros@gmail.com>
2018-05-12 06:39:37 -0700

    about card redsign

--- client/src/components/topicFeed/topicInput.js
+++ client/src/components/topicFeed/topicInput.js
@@ -3,12 +3,12 @@ import './topicInput.css';
 
 export const TopicInput = props => {
   return (
-      <div class="col-md-12 col-sm-12">
+      <div className="col-md-12 col-sm-12">
                     
-      <ul class="list-group media-list media-list-stream mb-4 ">
+      <ul className="list-group media-list media-list-stream mb-4 ">
 
-        <li class="media list-group-item p-4 new-topic-box">
-          <div class="col-md-12 col-sm-12">
+        <li className="media list-group-item p-4 new-topic-box">
+          <div className="col-md-12 col-sm-12">
             <form accept-charset="UTF-8">
                 <textarea className="expand form-control message-input-button-position" id="topic-text-box" name="text" placeholder="Type in your topic" rid="topic-text-box" {...props} rows="4" />
             </form>
